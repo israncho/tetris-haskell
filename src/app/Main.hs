d0ac46137ff7d173e918750c95041fab85da688a
israncho <isragutierrez@ciencias.unam.mx>
2023-04-25 23:41:39 -0600

    mejor organizado el proyecto

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -3,8 +3,7 @@ module Main where
 import Graphics.Gloss
 
 main :: IO ()
-main = display (InWindow "Mi ventana" (640, 480) (0, 0)) white drawingFunction
+main = display (InWindow "Tetris" (640, 480) (0, 0)) white drawingFunction
 
 drawingFunction :: Picture
 drawingFunction = translate (-50) 50 $ color red $ circle 80
-
