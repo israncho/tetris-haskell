efb20f9b8e2567a903ffcc8b9862ed4a1e12b3de
israncho <isragutierrez@ciencias.unam.mx>
2023-04-27 01:51:33 -0600

    vamo avanzando con el tablero

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -2,8 +2,38 @@ module Main where
 
 import Graphics.Gloss
 
-main :: IO ()
-main = display (InWindow "Tetris" (640, 480) (0, 0)) white drawingFunction
+wWidth, wHeight, cellSize :: Int
+
+-- | window width
+wWidth = 300
+
+-- | window height
+wHeight = 600
+
+-- | cell size
+cellSize = 30
+
+halfWW, halfWH :: Float
+
+-- | half of the width
+halfWW = fromIntegral wWidth / 2
+
+-- | half of the height
+halfWH = fromIntegral wHeight / 2
+
+gridWidth, gridHeight :: Int
+gridWidth = 10
+gridHeight = 20
 
 drawingFunction :: Picture
 drawingFunction = translate (-50) 50 $ color red $ circle 80
+
+linePicture :: [Point] -> Picture
+linePicture points = color red (line points)
+
+main :: IO ()
+main =
+  display
+    (InWindow "Tetris" (wWidth, wHeight) (50, 50))
+    black
+    (translate (-halfWW) (-halfWH) (linePicture [(0, 0), (100, 100)]))
