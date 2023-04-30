8a63ca09e4547039931aedc2333ce1d851fd61c2
israncho <isragutierrez@ciencias.unam.mx>
2023-04-29 19:32:09 -0600

    doc

--- src/Board.hs
+++ src/Board.hs
@@ -9,25 +9,26 @@ ceilPoints = [(x * 30, 600) | x <- [0 .. 10], x * 30 `mod` 30 == 0]
 leftPoints = [(0, x * 30) | x <- [0 .. 20], x * 30 `mod` 30 == 0]
 rightPoints = [(300, x * 30) | x <- [0 .. 20], x * 30 `mod` 30 == 0]
 
+-- | Take two lists of points and returns a list of paths.
 myZipp :: [(Int, Int)] -> [(Int, Int)] -> [[(Float, Float)]]
 myZipp [] _ = []
-myZipp ((a, b) : xs) ((c, d) : ys) = [(iToF a, iToF b), (iToF c, iToF d)] : myZipp xs ys
+myZipp ((a, b) : xs) ((c, d) : ys) = map tcast [(a, b), (c, d)] : myZipp xs ys
+  where
+    tcast (x, y) = (fromIntegral x :: Float, fromIntegral y :: Float)
 
-iToF :: Int -> Float
-iToF n = fromIntegral n :: Float
-
+-- | Lists of paths to draw the grid.
+hLines, vLines :: [[(Float, Float)]]
 hLines = myZipp leftPoints rightPoints
-
 vLines = myZipp basePoints ceilPoints
 
 grey :: Color
 grey = makeColor 0.5 0.5 0.5 1.0
 
--- | Given the lists of points returns the pictures to draw the grid.
+-- | Given a list of paths(lines) returns the pictures to draw all the paths.
 drawGrid :: [[Point]] -> [Picture]
 drawGrid = foldr ((:) . color grey . line) []
 
--- | Grid picture 
+-- | Grid picture
 grid :: Picture
 grid = pictures $ drawGrid (vLines ++ hLines)
 
--- src/app/Main.hs
+++ src/app/Main.hs
@@ -1,39 +1,25 @@
 module Main where
 
-import System.IO
-
 import Board
 import Graphics.Gloss
-import Graphics.Gloss.Interface.IO.Game (playIO, Event)
+import Graphics.Gloss.Interface.IO.Game (Event, playIO)
 
-
-wWidth, wHeight, cellSize :: Int
-
--- | window width
+-- | Constants for the display
+wWidth, wHeight :: Int
 wWidth = 300
-
--- | window height
 wHeight = 600
 
--- | cell size
-cellSize = 30
-
+-- | Constants for the display
 halfWW, halfWH :: Float
-
--- | half of the width
 halfWW = fromIntegral wWidth / 2
-
--- | half of the height
 halfWH = fromIntegral wHeight / 2
-
 
 -- | Display of the tetris.
 tetrisDisplay :: Display
 tetrisDisplay = InWindow "Tetris" (wWidth + 1, wHeight + 1) (200, 200)
 
-
 drawBoard :: () -> IO Picture
-drawBoard _ =  return (translate (-halfWW) (-halfWH) (pictures [square (30, 30) red, grid]))
+drawBoard _ = return (translate (-halfWW) (-halfWH) (pictures [square (30, 30) red, grid]))
 
 handleEvents :: Event -> () -> IO ()
 handleEvents _ _ = return ()
@@ -43,5 +29,4 @@ updateGame _ gameState = return gameState
 
 main :: IO ()
 main =
-  playIO tetrisDisplay black 60 () drawBoard handleEvents updateGame 
-
+  playIO tetrisDisplay black 60 () drawBoard handleEvents updateGame
