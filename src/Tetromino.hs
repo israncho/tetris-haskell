b564b98df4cfe467325a9bf551322c0ce2c5332a
israncho <isragutierrez@ciencias.unam.mx>
2023-05-04 04:53:29 -0600

    Funciones para mover tetrominos y

--- src/Tetromino.hs
+++ src/Tetromino.hs
@@ -1,10 +1,8 @@
 -- | Module for tetrominos
 module Tetromino where
 
+import Board
 import Graphics.Gloss
-
--- | Position in the board
-type Position = (Int, Int)
 
 -- | Names for tetrominos
 data Name = I | O | T | J | L | S | Z
@@ -21,7 +19,7 @@ data Tetromino = Tetromino
   }
   deriving (Eq, Show)
 
--- | Returns a tetromino in the initial position. 
+-- | Returns a tetromino in the initial position.
 newTetromino :: Name -> Tetromino
 newTetromino I = Tetromino I cyan [(3, 19), (4, 19), (5, 19), (6, 19)]
 newTetromino O = Tetromino O yellow [(4, 19), (4, 18), (5, 19), (5, 18)]
@@ -30,3 +28,13 @@ newTetromino J = Tetromino J blue [(3, 19), (4, 19), (5, 19), (5, 18)]
 newTetromino L = Tetromino L orange [(3, 19), (4, 19), (5, 19), (3, 18)]
 newTetromino S = Tetromino S green [(4, 18), (5, 18), (5, 19), (6, 19)]
 newTetromino Z = Tetromino S red [(4, 19), (5, 19), (5, 18), (6, 18)]
+
+-- | Functions to move a position by one.
+moveDown, moveLeft, moveRight :: Position -> Position
+moveDown (x, y) = (x, y - 1)
+moveLeft (x, y) = (x - 1, y)
+moveRight (x, y) = (x + 1, y)
+
+-- | Fuction to move a tetromino by one cell.
+move :: (Position -> Position) -> Tetromino -> Tetromino
+move movingFunct tetromino = tetromino {cells = map movingFunct (cells tetromino)}
