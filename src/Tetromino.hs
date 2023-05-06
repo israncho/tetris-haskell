d8f0b9d580d52a4969b1254534a194c695fecf40
israncho <isragutierrez@ciencias.unam.mx>
2023-05-06 03:06:20 -0600

    detalle

--- src/Tetromino.hs
+++ src/Tetromino.hs
@@ -27,7 +27,7 @@ newTetromino T = Tetromino T rose [(4, 19), (5, 19), (6, 19), (5, 18)]
 newTetromino J = Tetromino J blue [(3, 19), (4, 19), (5, 19), (5, 18)]
 newTetromino L = Tetromino L orange [(3, 19), (4, 19), (5, 19), (3, 18)]
 newTetromino S = Tetromino S green [(4, 18), (5, 18), (5, 19), (6, 19)]
-newTetromino Z = Tetromino S red [(4, 19), (5, 19), (5, 18), (6, 18)]
+newTetromino Z = Tetromino Z red [(4, 19), (5, 19), (5, 18), (6, 18)]
 
 -- | Functions to move a position by one.
 moveDown, moveLeft, moveRight :: Position -> Position
